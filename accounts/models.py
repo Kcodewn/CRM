aa4a77ff8668bfad3c898f78516fa94516dd9983
KnQ4 <kcodewn@gmail.com>
2022-07-31 15:05:20 -0500

    querried data

--- accounts/models.py
+++ accounts/models.py
@@ -12,17 +12,38 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+
+class Tag(models.Model):
+	name = models.CharField(max_length=10000, null=True)
+
+	def __str__(self):
+		return self.name
+
 class Product(models.Model):
 	CATEGORY = (
-			('Indoor', 'Indoor'),
-			('Out Door', 'Out Door'),
+			('Tanks', 'Tanks'),
+            ('Shorts', 'Shorts'),
+            ('Shirts', 'Shirts'),
+            ('Pants', 'Pants'),
+			('Jewelries', 'Jewelries'),
+            ('Hats', 'Hats'),
+            ('Shoes', 'Shoes'),
+            ('Makeups', 'Makeups'),
+
 			) 
 
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True)
+	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.name
+
+
+
 
 class Order(models.Model):
 	STATUS = (
@@ -31,7 +52,9 @@ class Order(models.Model):
 			('Delivered', 'Delivered'),
 			)
 
-	#customer = 
-	#product = 
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
+    
